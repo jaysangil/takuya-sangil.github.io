bb8aacc4e0ad82fa8f20bf078ae177cdebeda5f2
takuya-sangil <jayloveswonyoung@gmail.com>
2024-07-22 21:02:11 +0800

    temporary commit to switch branch

--- agent-monitoring-app/src/config/config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  clientId: '05e2fc28-91bc-4374-98ad-21ee9a0f19ac',
-  // redirectUri: 'https://genesyscloudblueprints.github.io/agent-monitoring-app-blueprint/'
-  redirectUri: 'https://jaysangil.github.io/takuya-sangil.github.io/agent-monitoring-app'
-}
